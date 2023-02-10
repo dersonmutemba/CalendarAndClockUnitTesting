0e008337ec27c5161c5a930baa722d2df4af8a40
dersonmutemba <dersonmutemba@gmail.com>
2023-02-10 11:34:49 -0600

    created date class

---
+++ CalendarLibrary/Date.cs
@@ -0,0 +1,12 @@
+namespace CalendarLibrary;
+
+public class Date
+{
+    public Date(string date) { }
+
+    public Date(int day, int month, int year) { }
+
+    public int Day { get; set; }
+    public int Month { get; set; }
+    public int Year { get; set; }
+}
