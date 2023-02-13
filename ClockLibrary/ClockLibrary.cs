5e3bfb09d412f1c8a48b12d76f8a8cbe072b09f1
dersonmutemba <dersonmutemba@gmail.com>
2023-02-13 11:37:55 -0600

    created clocklibrary class

---
+++ ClockLibrary/ClockLibrary.cs
@@ -0,0 +1,16 @@
+namespace ClockLibrary;
+
+public class ClockLibrary
+{
+    public static int Difference(Time time1, Time time2) {
+        throw new NotImplementedException();
+    }
+
+    public static Time Add(Time time, int minutes) {
+        throw new NotImplementedException();
+    }
+
+    public static Time Subtract(Time time, int minutes) {
+        throw new NotImplementedException();
+    }
+}
